26e5aeff207aea5229d941fef5ba36f2eaafd131
Gert van Kempen <g.vankempen@silverside.com>
2017-10-03 12:55:33 +0200

    Updated CSS url

--- static/profiles/profilesCustomization.js
+++ static/profiles/profilesCustomization.js
@@ -6,7 +6,7 @@
 //
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/profiles/profilesCustomization.css\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/profiles/profilesCustomization.css?repoName=silverside-customizer\"></link>",
         dojo.doc.head,
         "last"
     );
